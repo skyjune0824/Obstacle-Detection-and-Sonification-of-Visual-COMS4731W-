a6b2c038a8142f8a6b5027ae0008705730f3ed69
cscaff <cscaff@ufl.edu>
2025-10-25 20:14:55 -0400

    Added Depth Anything V2 Model + Reformatted project structure.

---
+++ src/depth_estimation/estimator.py
@@ -0,0 +1,9 @@
+# Things to take into account for the model.
+# Average walking speed: 1.10 m/s - 1.65 m/s: Avg: 1.4 m/s
+# Starting Intuition: 1 frame per second (1 frame every 1.4 meters).
+# Need a model capable of computing one depth map per second with negligible latency.
+# https://huggingface.co/apple/coreml-depth-anything-v2-small - Depth Anything V2 optimized for mobile development
+# iPhone 12 Pro Max - Inference time = 31.10 MS -> Assuming inference grows at a scale much larger than the other aspects of our computations,
+# We could in theory take up to 30 images per second.
+# Because Depth Anything V2 is supported by CoreML and we are considering a mobile implementation, we will use this model.
+
