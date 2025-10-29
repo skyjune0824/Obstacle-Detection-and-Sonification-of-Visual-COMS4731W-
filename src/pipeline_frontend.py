8820bcaebb883fa1db84230617d69c5ca0e92e08
cscaff <cscaff@ufl.edu>
2025-10-29 15:47:24 -0400

    Added Video to Image Sampling.

---
+++ src/pipeline_frontend.py
@@ -0,0 +1,42 @@
+import cv2
+import os
+
+def sample_frames(video_path, output_dir):
+    """
+    Samples frames from video input for depth detection.
+    
+    """
+
+    # Generates outout directory.
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Capture video
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        print(f"Error: Could not open video file {video_path}")
+        return
+    
+    frame_count = 0
+    saved_count = 0
+    frame_interval = 30
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        if frame_count % frame_interval == 0:
+            frame_filename = f"frame_{saved_count:05d}.jpg"
+            output_path = os.path.join(output_dir, frame_filename)
+            cv2.imwrite(output_path, frame)
+            saved_count += 1
+        
+        frame_count += 1
+
+    cap.release()
+    print(f"Finished sampling. Saved {saved_count} frames to '{output_dir}'.")
+
+if __name__ == "__main__":
+    input_video = "/Users/christianscaff/Documents/Academics/Columbia/Fall_25/COMS_4731/Project/Data/IMG_2200.MOV"
+    output_folder = "/Users/christianscaff/Documents/Academics/Columbia/Fall_25/COMS_4731/Project"
+    sample_frames(input_video, output_folder)
