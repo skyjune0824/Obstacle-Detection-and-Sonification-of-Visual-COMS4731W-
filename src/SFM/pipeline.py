cc57bbeb682849b92aeabd4ed4c87d559a18eea4
cscaff <cscaff@ufl.edu>
2025-11-23 08:16:05 -0500

    Added Orb Matching between frames.

--- src/SFM/pipeline.py
+++ src/SFM/pipeline.py
@@ -43,13 +43,14 @@ class SFM_Pipeline:
         # Core Pipeline Loop
         while True:
             # Read Frame While Possible
-            ret, frame = cap.read()
+            ret, frame_one = cap.read()
+            ret, frame_two = cap.read()
             if not ret:
                 break
 
             # Sample and Process Frame in multiples of "Sample Rate". 
             if frame_cnt % self.sample_rate == 0:
-                self.process(frame)
+                self.process(frame_one, frame_two)
 
             # Increment Frame Count
             frame_cnt += 1
@@ -61,7 +62,7 @@ class SFM_Pipeline:
 
         # return self.audio_trace
     
-    def process(self, frame):
+    def process(self, frame_one, frame_two):
         """ Process
         Denotes one singular pass in the pipeline.
         1. Captures Depth Map
@@ -71,13 +72,33 @@ class SFM_Pipeline:
         """
 
         # Preprocess
-        img = cv2.imread(frame)
-
         # Extract ORB Features
         orb = cv2.ORB_create(nfeatures=5000)
 
-        # Get Key Points
-        keypoints, descriptors = orb.detectAndCompute(img, None)
+        # Get Key Points Between Images
+        keypoint_one, descriptors_one = orb.detectAndCompute(frame_one, None)
+        keypoint_two, descriptors_two = orb.detectAndCompute(frame_two, None)
+
+        # Match Descriptors Between Images
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        matches = bf.match(descriptors_one, descriptors_two)
+
+        # Sort Matches
+        matches = sorted(matches, key=lambda x: x.distance)
+
+        if DEBUG:
+            match_img = cv2.drawMatches(
+                frame_one, keypoint_one,
+                frame_two, keypoint_two,
+                matches[:50],
+                None,
+                flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS
+            )
+            cv2.imshow("Feature Matches", match_img)
+            cv2.waitKey(0)
+
+
+    # def track_features()
 
 
 def log(msg):
