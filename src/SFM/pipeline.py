48d703b529f82ec238feec5950b0e142560645a1
cscaff <cscaff@ufl.edu>
2025-11-23 10:38:22 -0500

    Basic Triangulation

--- src/SFM/pipeline.py
+++ src/SFM/pipeline.py
@@ -4,6 +4,7 @@ from config import DEBUG
 # Video Processing
 import cv2
 from PIL import Image
+import numpy as np
 
 # Modules
 from src.AudioSynthesis.synthesize import SpatialAudioFeedback
@@ -48,9 +49,18 @@ class SFM_Pipeline:
             if not ret:
                 break
 
+            # Initial Pose
+            pose_init = np.eye(4)
+
             # Sample and Process Frame in multiples of "Sample Rate". 
             if frame_cnt % self.sample_rate == 0:
-                self.process(frame_one, frame_two)
+                # Calculate Pose
+                new_pose, pts_one, pts_two = self.process_trajectory(frame_one, frame_two)
+                curr_pose = pose_init @ new_pose
+
+                # Triangulate
+                self.triangulate(pose_init, curr_pose, pts_one, pts_two)
+
 
             # Increment Frame Count
             frame_cnt += 1
@@ -62,7 +72,7 @@ class SFM_Pipeline:
 
         # return self.audio_trace
     
-    def process(self, frame_one, frame_two):
+    def process_trajectory(self, frame_one, frame_two):
         """ Process
         Denotes one singular pass in the pipeline.
         1. Captures Depth Map
@@ -97,10 +107,71 @@ class SFM_Pipeline:
             cv2.imshow("Feature Matches", match_img)
             cv2.waitKey(0)
 
+        # Match Points
+        pts_one = np.float32([keypoint_one[m.queryIdx].pt for m in matches])
+        pts_two = np.float32([keypoint_two[m.trainIdx].pt for m in matches])
+        
+        # Approximated Intrinsic for iPhone 14
+        K = np.array([[780, 0, 540],
+              [0, 780, 960],
+              [0, 0, 1]])
+        
+        # Fundamental Matrix
+        F, inliers = cv2.findFundamentalMat(pts_one, pts_two, cv2.FM_RANSAC)
 
-    # def track_features()
+        # Essential Matrix
+        E = K.T @ F @ K
+
+        # Get Points for Inliner Matches
+        pts_one_inliers = pts_one[inliers.ravel() == 1]
+        pts2_two_inliers = pts_two[inliers.ravel() == 1]
+
+        # Decompose for Rotation + Translation
+        _, R, t, mask = cv2.recoverPose(E, pts_one_inliers, pts2_two_inliers, K)
+
+        # Construct Pose Matrix
+        pose = np.eye(4)
+        pose[:3, :3] = R
+        pose[:3, 3] = t.ravel()
+
+        return pose, pts_one_inliers, pts2_two_inliers
 
 
+    def triangulate(self, pose1, pose2, pts1, pts2):
+        # Initialize array to store homogeneous coordinates of 3D points
+        points_3d_h = np.zeros((pts1.shape[0], 4))
+    
+        # Invert camera poses to convert World Points to Camera Points
+        pose1 = np.linalg.inv(pose1)
+        pose2 = np.linalg.inv(pose2)
+    
+        # Loop through each pair of corresponding points
+        for i, p in enumerate(zip(np.hstack([pts1, np.ones((pts1.shape[0], 1))]), np.hstack([pts2, np.ones((pts2.shape[0], 1))]))):
+            # Initialize matrix A w/ linear equations: X = PX
+            A = np.zeros((4, 4))
+    
+            # Populate the matrix A with equations derived from projection matrices + pts
+            A[0] = p[0][0] * pose1[2] - pose1[0]
+            A[1] = p[0][1] * pose1[2] - pose1[1]
+            A[2] = p[1][0] * pose2[2] - pose2[0]
+            A[3] = p[1][1] * pose2[2] - pose2[1]
+    
+            # SVD on A
+            _, _, vt = np.linalg.svd(A)
+    
+            # Last row of (V^T) - smallest singular value = Triangulated Solution
+            points_3d_h[i] = vt[3]
+
+        # Divide Out W from [X, Y, Z, W] and remove invalid points.
+        valid_filter = (np.abs(points_3d_h[:, 3]) > 0.005) & (points_3d_h[:, 2] > 0)
+        pts3d_h_filtered = points_3d_h[valid_filter]  
+        pts3d = pts3d_h_filtered[:, :3] / pts3d_h_filtered[:, 3][:, np.newaxis]
+    
+        # Return the 3D points
+        log(f"Triangulated Points: {pts3d}")
+
+        return pts3d
+ 
 def log(msg):
     if DEBUG:
         print(msg)
