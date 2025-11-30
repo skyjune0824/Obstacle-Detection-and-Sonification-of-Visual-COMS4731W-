244013993793ddefa39c49ce0629987f8a73bef4
cscaff <cscaff@ufl.edu>
2025-11-30 10:36:00 -0500

    Cleaned up SLAM Pipeline.

--- src/SLAM/pipeline.py
+++ src/SLAM/pipeline.py
@@ -16,11 +16,22 @@ class SFM_Pipeline:
     estimating object distance for sonification.
     """
 
-    def __init__(self, rate, K):
+    def __init__(self, K, cam_speed=1.4, local=True):
+        """ SFM Pipeline Constructor
+
+        1. Initializes Audio Class
+        2. Sets Camera Intrinsics  
+        3. Sets Camera Speed. We default to average walking speed.
+        4. Switch between Local and Global for Local (Faster, Sparse) Versus Global (Slower, Dense) Point Maps
+        5. Initialize CV Capture
+        6. Initialize World Map 
+        """
+
         self.synth = SpatialAudioFeedback()
-        self.sample_rate = rate
-        self.cap = None
         self.K = K
+        self.cam_speed = cam_speed
+        self.local = local
+        self.cap = None
         self.world_map = []
 
 
@@ -30,11 +41,9 @@ class SFM_Pipeline:
         from multiple images in motion, performing segmentation, and synthesizing spatial audio.
 
         Source: Path to video we desire to perform our pipeline on.
-        Debug: Toggles Verbose
-
         """
 
-        print(f"Performing SFM on video located at: {source}")
+        log(f"Performing SFM on video located at: {source}")
 
         # Open Video Path
         self.cap = cv2.VideoCapture(source)
@@ -56,7 +65,6 @@ class SFM_Pipeline:
             if not ret:
                 break
 
-            # Sample and Process Frame in multiples of "Sample Rate". 
             if prev_frame is not None:
                 # Calculate Pose
                 relative_pose, pts_one, pts_two = self.process_trajectory(prev_frame, cur_frame)
@@ -69,20 +77,32 @@ class SFM_Pipeline:
                     local_pose1 = np.eye(4)
                     local_points = self.triangulate(local_pose1, relative_pose, pts_one, pts_two)
 
-                    # Convert Local Points to World Points
-                    world_points_current = self.transform_to_world(local_points, prev_pose)
+                    # Global Point Map Algo
+                    if not self.local:
+                        # Transform Local Points to World Coordinates
+                        world_points_current = self.transform_to_world(local_points, prev_pose)
 
-                    # Add to map
-                    if world_points_current.any():
+                        # Add to World Map
                         self.world_map.extend(world_points_current)
-                        log(f"World Map Added {len(world_points_current)}. TOTAL SIZE: {len(self.world_map)}")
 
-                    # Find Distance to closest object in each zone.
-                    minimum, min_pnt = self.min_distance(curr_pose[:3, 3])
+                        # Find Distance to Closest Point in World Coordinates.
+                        minimum, min_pnt = self.min_world_distance(curr_pose[:3, 3])
 
-                    # Visualize
-                    if DEBUG and min_pnt is not None and local_points.any():
-                        self.viz_dist_points(min_pnt, minimum, curr_pose, cur_frame)
+                        # Clip World Map to Fresh Points
+                        if len(self.world_map) > 100:
+                            self.world_map = self.world_map[-100:]
+
+                        # Visualize
+                        if DEBUG and min_pnt is not None:
+                            self.viz_world_points(min_pnt, minimum, curr_pose, cur_frame)
+                    # Local Point Map Algo
+                    else:
+                        # Get Local Min Distance
+                        minimum, min_pnt = self.min_local_distance(local_points)
+
+                        # Visualize
+                        if DEBUG and min_pnt is not None and local_points.any():
+                            self.viz_local_points(min_pnt, minimum, local_points, cur_frame)
 
                 # Set Prev Pose
                 prev_pose = curr_pose
@@ -99,12 +119,14 @@ class SFM_Pipeline:
         # return self.audio_trace
     
     def process_trajectory(self, frame_one, frame_two):
-        """ Process
-        Denotes one singular pass in the pipeline.
-        1. Captures Depth Map
-        2. Segments Depth Map and creates BEV.
-        3. Synthesizes into Spatial Audio.
-        
+        """ Process Trajectory
+
+        1. Get Orb Features
+        2. Determine Best Matches
+        3. Calculate Essential Matrix 
+        4. Filter Outliers
+        5. Normalize Points and Construct Pose
+        6. Scale Pose to Camera Speed
         """
 
         # Extract ORB Features
@@ -154,27 +176,28 @@ class SFM_Pipeline:
 
         # Scale Pose to Walking Speed
         fps = self.cap.get(cv2.CAP_PROP_FPS)
-        walking_speed = 1.4  # meters per second 
-        estimated_scale_translation = walking_speed * (1 / fps)
+        estimated_scale_translation = self.cam_speed * (1 / fps)
         pose[:3, 3] *= estimated_scale_translation
 
         return pose, pts_one_normed, pts_two_normed
 
 
-    def triangulate(self, pose1, pose2, pts1, pts2):    
+    def triangulate(self, pose1, pose2, pts1, pts2):  
+        """ Triangulate
+
+        1. Utilizes current and previous pose as well as current and previous points to perform
+        triangulation via epipolar geometry.
+        2. Filters out invalid points.
+        3. Transforms from homogenous to 3D.
+        """  
+
         # Invert camera poses to convert World Points to Camera Points
         pose1 = pose1[:3, :]
         pose2 = pose2[:3, :]
-
-        # DEBUG
-        # if DEBUG:
-        #     log(f"POSE1: {pose1},\n POSE2: {pose2}")
-        #     log(f"pts1: {pts1.T}, pts2: {pts2.T}")
     
         points_4d = cv2.triangulatePoints(pose1, pose2, pts1.T, pts2.T)
         points_4d = points_4d.T
 
-        # log(f"Before Filter: {len(points_4d)}")
         # Filter and Divide Out W from [X, Y, Z, W]
         valid_filter = (np.abs(points_4d[:, 3]) > 0.005)
         points_4d = points_4d[valid_filter]
@@ -186,18 +209,15 @@ class SFM_Pipeline:
         reasonable_depth = (points_3d[:, 2] < 80.0) & (points_3d[:, 2] > 0.1)
         points_3d = points_3d[reasonable_depth]
 
-        # log(f"After Filter: {len(points_3d)}")
-
-        # if DEBUG and points_3d.any():
-        #     log(f"Triangulated {len(points_3d)} points")
-        #     log(f"Z range: {points_3d[:, 2].min():.2f} to {points_3d[:, 2].max():.2f}")
-        #     log(f"XY range: X[{points_3d[:, 0].min():.2f}, {points_3d[:, 0].max():.2f}], "
-        #         f"Y[{points_3d[:, 1].min():.2f}, {points_3d[:, 1].max():.2f}]")
-                
         # Return the 3D points
         return points_3d
     
     def transform_to_world(self, local_points, camera_pose):
+        """ Transform To World
+
+        1. Transforms Local Points to World Coordinates
+        """
+
         if len(local_points) == 0:
             return np.array([])
 
@@ -211,7 +231,12 @@ class SFM_Pipeline:
         # Return 3D coordinates
         return world_homogeneous[:, :3]
 
-    def min_distance(self, cam_pos, count=100):
+    def min_world_distance(self, cam_pos, count=100):
+        """ Min World Distance
+        
+        1. Finds minimum position on front of camera in world point map.
+        2. Returns that distance and the point coordinates.
+        """
         if len(self.world_map) > 0:
             world_pts_array = np.array(self.world_map[-count:])
 
@@ -229,27 +254,54 @@ class SFM_Pipeline:
   
         return float('inf'), None
     
+    def min_local_distance(self, points):
+        """ Min Local Distance
+        
+        1. Finds minimum position in local point map.
+        2. Returns minimum position distance and point coordinates.
+        """
+        if points.shape[0] > 0:
+            cam_pos_local = np.array([0, 0, 0])
+            dists = np.linalg.norm(points - cam_pos_local, axis=1)        
+            min_idx = np.argmin(dists)
+            min_dist = dists[min_idx]
+            min_pnt = points[min_idx]
 
-    def transform_to_local(self, camera_pose, point): 
-        # Invert Pose
-        camera_pose_inv = np.linalg.inv(camera_pose)
+            return min_dist, min_pnt
+
+        return float('inf'), None
+    
+
+    def transform_to_local(self, cam_pos, points):
+        """ Transform to Local
+
+        1. Converts set of World Points to Local Points
+        """
+        if len(points) == 0:
+            return np.array([])
         
-        # Convert point to homogeneous coordinates
-        point_homogeneous = np.append(point, 1)
-        
+        world_pts_array = np.array(points)
+        # Inverse Camera Pose -> World-to-Camera
+        camera_pose_inv = np.linalg.inv(cam_pos)
+        # Homogenous Coords
+        ones = np.ones((world_pts_array.shape[0], 1))
+        world_homogeneous = np.hstack([world_pts_array, ones])
         # Transform
-        camera_homogeneous = camera_pose_inv @ point_homogeneous
-        
-        # Return 3D
-        return camera_homogeneous[:3]
+        camera_homogeneous = (camera_pose_inv @ world_homogeneous.T).T
+
+        # 3D Coords
+        return camera_homogeneous[:, :3]
 
     
-    def viz_dist_points(self, min_pnt, min_dist, cur_pose, cur_frame):
-        # Get Local Points In Frame
-        cam_points = []
+    def viz_world_points(self, min_pnt, min_dist, cur_pose, cur_frame):
+        """ Visualize World Points
 
-        for point in self.world_map[-50:]:
-            cam_points.append(self.transform_to_local(cur_pose, point))
+        1. Transforms World Points to Local Points. 
+        2. Transforms Minimum Point to Local Point.
+        3. Calls Local Visualization.
+        """
+        # Get Local Points In Frame
+        cam_points = self.transform_to_local(cur_pose, self.world_map)
 
         cam_points = np.vstack(cam_points)
 
@@ -260,21 +312,30 @@ class SFM_Pipeline:
         in_front = cam_points[:, 2] > 0.1
         camera_points_front = cam_points[in_front]
 
+        self.viz_local_points(self, min_point, min_dist, camera_points_front, cur_frame)
+
+    def viz_local_points(self, min_pnt, min_dist, points, cur_frame):
+        """ Visualize Local Points
+
+        1. Converts local points to pixel coordinates.
+        2. Displays points.
+        """
+        ret = np.dot(self.K, min_pnt)
+        ret /= ret[2]
+
+        x = int(round(ret[0]))
+        y = int(round(ret[1]))
+
+        log(f"MIN DIST: {min_dist} @ ({x}, {y})")
+
         # All Triangulated Positions
-        for pnt in camera_points_front:
+        for pnt in points:
             ret = np.dot(self.K, pnt)
             ret /= ret[2]
 
             x = int(round(ret[0]))
             y = int(round(ret[1]))
             cv2.circle(cur_frame, (x, y), 8, (0, 255, 0), -1)
-
-        # Get Min Point
-        ret = np.dot(self.K, min_point)
-        ret /= ret[2]
-
-        x = int(round(ret[0]))
-        y = int(round(ret[1]))
 
         cv2.circle(cur_frame, (x, y), 8, (0, 0, 255), -1)
         cv2.putText(
@@ -287,20 +348,10 @@ class SFM_Pipeline:
             2
         )
 
-        log(f"MIN DIST: {min_dist} @ ({x}, {y})")
-
-
         cv2.imshow("Closest Point Visualization", cur_frame)
         cv2.waitKey(10)
-
 
  
 def log(msg):
     if DEBUG:
         print(msg)
-
-# Notes
-# I could try and add all the points and poses to an accumulating map
-# and calculate each pose relative to the world position instead of local and see 
-# if that improved distance accuracy (Pull min distance from overall map)
-# I could also take clusters or median distances
